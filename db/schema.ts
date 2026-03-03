57de719403b59e177f29c56af5c23f790eade843
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-03-03 13:47:50 +0530

    create schema

---
+++ db/schema.ts
@@ -0,0 +1,65 @@
+import { pgTable,integer, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+export {db} from "@/db"
+import {user} from "./auth-schema"
+
+export const customer = pgTable("customer",{
+userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+  id: text("id").primaryKey(),
+  name:text("name").notNull(),
+  phoneNo:text("phone_no").notNull(),
+  address:text("address").notNull(),
+})
+
+export const allUdhar = pgTable("allUdhar",{
+    id: text("id").primaryKey(),
+    customerId:text("customer_id").notNull().references(()=>customer.id ,{ onDelete : "cascade"}),
+    date:timestamp("date").defaultNow().notNull(),
+    product:text("product_name").notNull(),
+    qty:integer().notNull(),
+    price:integer().notNull(),
+    totalprice:integer().notNull(),   // qty x price
+
+})
+
+export const inventory = pgTable("inventory", {
+  id: text("id").primaryKey(),
+  userId: text("user_id").notNull(),
+
+  productName: text("product_name").notNull(),
+  stock: integer("stock").notNull(),
+  price: integer("price").notNull(),
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const function_name = async () =>{
+//     try {
+        
+//     } catch (error) {
+//         if( error instanceof Error){
+//         return { message:error.message ,status:500}
+//         }
+//     }
+// }
+// // export const function_name = async () =>{
+// //     try {
+        
+// //     } catch (error) {
+// //         if( error instanceof Error){
+// //         return { message:error.message ,status:500}
+// //         }
+// //     }
+// // }
